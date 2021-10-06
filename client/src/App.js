78df9bc726d529fa12ed9938724fd70b7f3c0004
Sungoh Park <spark449062@gmail.com>
2021-10-06 11:05:34 -0600

    routing working in progress

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
+import React from "react";
 import { useState, useEffect } from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route, NavLink } from "react-router-dom";
+import Card from "./components/Card"
 
 function App() {
   const [count, setCount] = useState(0);
@@ -13,13 +15,30 @@ function App() {
   return (
     <BrowserRouter>
       <div className="App">
+
+        <NavLink activeClassName="active-nav-link" exact to="study">
+          Study
+        </NavLink>
+
+        <NavLink activeClassName="active-nav-link" exact to="quiz">
+          Quiz
+        </NavLink>
+
         <Switch>
-          <Route path="/testing">
-            <h1>Test Route</h1>
-          </Route>
+
           <Route path="/">
             <h1>Page Count: {count}</h1>
           </Route>
+
+          <Route exact path="/study">
+            <h1>Study Session</h1>
+            <Card/>
+          </Route>
+
+          <Route exact path="/quiz">
+            <h1>Quiz Session</h1>
+          </Route>
+
         </Switch>
       </div>
     </BrowserRouter>
---
+++ client/src/components/Card.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+function Card() {
+
+    return <h1>This is Card</h1>
+}
+
+export default Card
