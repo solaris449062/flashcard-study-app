ccbb6d7770a38274fdbfa0d580a0ce2704f10eb8
Jeff Thomassen <jeff.thomassen@ucdenver.edu>
2021-10-07 15:14:37 -0600

    removed header from login

--- client/src/components/Quiz.js
+++ client/src/components/Quiz.js
@@ -1,9 +1,11 @@
 import React from "react";
 import NavBar from "./NavBar";
+import Header from "./Header";
 
-function Quiz() {
+function Quiz({user, onLogout}) {
     return (
         <div>
+            <Header user={user} onLogout={onLogout}/>
             <NavBar/>
             <h1>Quiz Session</h1>
         </div>
